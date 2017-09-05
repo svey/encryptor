3e0af1ddb6a55ae8e7941e0548dcdf0bdab5c30e
svey <marcus.svehlak@gmail.com>
2017-09-05 13:01:55 -0700

    Created message type for graphql build schema

--- server.js
+++ server.js
@@ -1,23 +1,56 @@
-var { buildSchema } = require('graphql');
+var { buildSchema, GraphQLObjectType, GraphQLString } = require('graphql');
 var CryptoJS = require("crypto-js");
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var path = require('path')
 
+var messageCache = {};
+
+// var MessageType = new GraphQLObjectType({
+//   name: 'Message',
+//   fields: {
+//     name: { type: GraphQLString },
+//     message: { type: GraphQLString }
+//   }
+// })
+
+class Message {
+  constructor(name, message) {
+    this.name = name;
+    this.message = message;
+  }
+}
+
 var schema = buildSchema(`
+  type Message {
+    name: String
+    message: String
+  }
+
   type Query {
-    encrypt(text: String!): String
-    decrypt(text: String!): String
+    encrypt(text: String!, date: String!, name: String! ): String
+    decrypt(text: String!): Message
   }
 `);
 
 var root = {
-  encrypt: ({ text }) => {
-    return CryptoJS.AES.encrypt(text, "Secret Passphrase");
+  encrypt: ({ text, date, name }) => {
+    var encryption = CryptoJS.AES.encrypt(text, 'secret');
+    messageCache[encryption] = { date, name };
+    return encryption;
   },
   decrypt: ({ text }) => {
-    return CryptoJS.AES.decrypt(text, "Secret Passphrase").toString(CryptoJS.enc.Utf8);
+    var { date, name } = messageCache[text];
 
+    var expirationDate = date.slice(0, 10); //slice at 10 to ignore time of day
+    var currentDate = new Date().toISOString().slice(0, 10); //slice at 10 to ignore time of day
+    var message = CryptoJS.AES.decrypt(text, 'secret').toString(CryptoJS.enc.Utf8)
+    console.log(name, message, date)
+    if(currentDate <= expirationDate) {
+      return new Message(name, message);
+    }
+
+    return 'The message has either expired or the encryption was invalid.'
   }
 };
 
